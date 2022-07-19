e9a6166970c70454492ee31a79165bb2ceda8bae
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-07-19 18:10:24 +0300

    Add test inputs

--- longestValidString.js
+++ longestValidString.js
@@ -4,9 +4,11 @@ function longestWordInSentence(sentence) {
   for (let word of sentence) {
     if (word !== "") {
       word = word.match(/^[a-z]+|(\s+)/i)[0];
-      if (word && word.length > longestWord.length) {
+      if (word.length > longestWord.length) {
         longestWord = word;
-      } else if (word.trim().length === longestWord.trim().length) {
+      } else if(word.length < longestWord.length){
+        longestWord = longestWord;
+      }else if (word.trim().length === longestWord.trim().length) {
         if (
           word.trim() &&
           longestWord.trim() &&
@@ -31,4 +33,12 @@ let input2 =  '          Given a sentence with multiple lowercase words separate
 
 let input3 = 'Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers';
 
-console.log(longestWordInSentence(input3));
+let input4 = 'Smart people learn from everything           and everyone, average people from their experience, stupid people already, have all the answers';
+
+let input5 = 'Smart people learn from everything          and everyone, average people from their experience, stupid people already, have all the answers';
+
+console.log(longestWordInSentence(input1)); //Javascript
+console.log(longestWordInSentence(input2)); //Javascript
+console.log(longestWordInSentence(input3)); //experience
+console.log(longestWordInSentence(input4)); //'           '
+console.log(longestWordInSentence(input5)); //experience
